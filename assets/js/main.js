cee76a7c0de1f6c3bbe4f1f5c18c6ec41eacd0ac
Mike <aolie94@gmail.com>
2017-03-30 16:15:53 -0700

    revised html layout and added parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+    $("[data-media]").on("click", function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var videoUrl = $this.attr("data-media");
+        var popup = $this.attr("href");
+        var $popupIframe = $(popup).find("iframe");
+
+        $popupIframe.attr("src", videoUrl);
+
+        $this.closest(".page").addClass("show-popup");
+    });
+
+    $(".popup").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        $(".page").removeClass("show-popup");
+    });
+
+    $(".popup > iframe").on("click", function(e) {
+        e.stopPropagation();
+    });
+
     $(window).scroll(function() {
         if ($(window).scrollTop() < 450) {
             $(".text-wrapper").fadeOut(function() {
