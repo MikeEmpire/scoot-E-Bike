3d8be83c850e6085eb038bbb6005aa8747ab93b1
Mike <aolie94@gmail.com>
2017-04-06 16:52:02 -0700

    revised html structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
-
+    var isScrolling = false;
     var video = $("#bgvid");
+    var nav = $("#topNav");
 
-    $('.tlt').textillate({ in: { effect: 'bounceInRight' } });
+    function isPartiallyVisible(el) {
+        var elementBoundary = el.getBoundingClientRect();
+
+        var top = elementBoundary.top;
+        var bottom = elementBoundary.bottom;
+        var height = elementBoundary.height;
+
+        return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+    }
+
+    function isFullyVisible(el) {
+        var elementBoundary = el.getBoundingClientRect();
+
+        var top = elementBoundary.top;
+        var bottom = elementBoundary.bottom;
+
+        return ((top >= 0) && (bottom <= window.innerHeight));
+    }
 
     function fadeIn() {
         $("#titleP").fadeIn("slow", function() {
             $("#titleP").css("display", "inherit");
         });
     }
+
+
+    $('.tlt').textillate({ in: { effect: 'bounceInRight' } });
 
     setTimeout(fadeIn, 5000);
 });
